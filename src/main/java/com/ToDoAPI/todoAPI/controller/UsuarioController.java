a8e9a3b1351b8f83006453b9448bd998533dd806
Filipe Formigari <formigari2804@gmail.com>
2024-11-26 20:25:37 -0300

    a

--- src/main/java/com/ToDoAPI/todoAPI/controller/UsuarioController.java
+++ src/main/java/com/ToDoAPI/todoAPI/controller/UsuarioController.java
@@ -3,11 +3,9 @@ package com.ToDoAPI.todoAPI.controller;
 import com.ToDoAPI.todoAPI.model.Usuario;
 import com.ToDoAPI.todoAPI.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -21,7 +19,29 @@ public class UsuarioController {
     public Usuario salvar(@RequestBody Usuario usuario) {
 
         usuario.setId_usuario(UUID.randomUUID());
-
         return usuarioRepository.save(usuario);
     }
+
+    @GetMapping
+    public List<Usuario> getAll() {
+        return usuarioRepository.findAll();
+    }
+
+    @GetMapping("{nome}")
+    public Usuario getOne(@PathVariable String nome){
+
+        return this.usuarioRepository.findByNome(nome);
+    }
+
+    @PutMapping
+    public Usuario update(@RequestBody Usuario usuario) {
+        return this.usuarioRepository.save(usuario);
+    }
+
+    @DeleteMapping
+    public void delete(@RequestBody Usuario usuario) {
+        this.usuarioRepository.delete(usuario);
+    }
+
+
 }
